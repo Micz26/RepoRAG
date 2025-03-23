9412e27cec976a175ff10c70c75b00a2db3ae0bd
Mikołaj Czachorowski <122210130+Micz26@users.noreply.github.com>
2025-03-24 00:33:30 +0100

    Quick fix retriever

--- src/repo_rag/components/retrievers.py
+++ src/repo_rag/components/retrievers.py
@@ -4,4 +4,4 @@ from repo_rag.components.vectorstore import Vectorstore
 vectorstore = Vectorstore(1)
 vectorstore.create()
 vectorstore = vectorstore.load()
-retriever = vectorstore.as_retriever(search_type='similarity', search_kwargs={'k': 50})
+retriever = vectorstore.as_retriever(search_type='similarity', search_kwargs={'k': 10})
